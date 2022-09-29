88f93720b18e12e89b865ac1f76fc23befb783b8
DuvanR11 <duriveramo@gmail.com>
2022-09-29 10:10:20 -0500

    ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProjectionComponent } from './components/projection/projection.component';
 import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectionComponent,
     ContentComponent
   ],
   imports: [
